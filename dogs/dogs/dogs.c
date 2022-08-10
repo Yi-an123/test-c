4e96df9db9bb38706342b7e9e7de87b3ed639155
Hi_Yan <1879486083@qq.com>
2022-08-10 19:40:38 +0800

    。

---
+++ dogs/dogs/dogs.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int dogs;
+	printf("How many dogs there?\n");
+		scanf("%d", &dogs);
+	printf("Wow,so you have %d dogs!\n", dogs);
+	return 0;
+}
