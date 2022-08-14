9eb81a25627d0e630522cea454b76439ccc8c2cf
Hi_Yan <1879486083@qq.com>
2022-08-14 21:44:46 +0800

    。

---
+++ age/age/age.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int ages;
+	printf("How old are you?\n"); 
+		scanf("%d", &ages);
+	printf("Wow,%d.You are so young!\n", ages);
+	return 0;
+}
