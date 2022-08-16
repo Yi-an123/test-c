4d282af52798a63aad6bec410ef7a235a5e41d07
Hi_Yan <1879486083@qq.com>
2022-08-16 17:33:46 +0800

    。

--- Project1/Project1/源.cpp
+++
@@ -1,8 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
-int main(void)
-{
-	printf("helllo world\n");
-	printf("cnm\n");
-	return 0;
-}
---
+++ add_2/add_2/add_2.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int b = 0;
+	int g = 0;
+	int sum = 0;
+	scanf("%d %d", &b, &g);
+	sum = b + g;
+	printf("sum = %d/n", sum);
+	return 0;
+}
