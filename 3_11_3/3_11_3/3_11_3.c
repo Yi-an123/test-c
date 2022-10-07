f220d45cb534090ab67b71ffde73721762aa043e
Hi_Yan <1879486083@qq.com>
2022-10-07 23:20:34 +0800

    .

---
+++ 3_11_3/3_11_3/3_11_3.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main(void)
+{
+	printf("\a");
+	printf("Startled by the sudden sound, Sally shouted,\"By the Great Pumpkin, what was that!\"\n");
+	return 0;
+}
